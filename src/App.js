6b33b659a4e5be4b5828dd168b046b0dbd5d5da0
Pankaj Papnai <engg.pankaj98@gmail.com>
2022-12-09 13:05:28 +0530

    Added pagination

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import React, { useState, useEffect } from "react";
 import Search from "./Components/Search/Search";
 import Card from "./Components/Card/Card";
 import Pagination from "./Components/Pagination/Pagination";
-import Filter from "./Components/Filter/Filter";
-import Navbar from "./Components/Navbar/Navbar";
+import "./App.css"
 
 
 
@@ -28,7 +27,6 @@ useEffect(()=>{
       <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
       <div className="row">
-      Filter component will be placed here
         <div className="col-lg-8 col-12">
           <div className="row">
             <Card results={results}/>
@@ -36,6 +34,7 @@ useEffect(()=>{
         </div>
       </div>
       </div>
+      <Pagination info={info} pageNumber={pageNumber} updatePageNumber={updatePageNumber}/>
     </div>
   );
 }
